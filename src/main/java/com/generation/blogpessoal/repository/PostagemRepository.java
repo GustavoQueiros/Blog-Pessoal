f91fc41ad5fdd9cd9ae5404b6effaf48263495c0
GustavoQueiros <gustavo.dev47@gmail.com>
2024-01-12 13:17:12 -0300

    ATV02

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -3,13 +3,16 @@ package com.generation.blogpessoal.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.generation.blogpessoal.model.Postagem;
 //JPA da o poder da a repository o poder de se comunicar com o Banco de dados
+
+@Repository
 public interface PostagemRepository  extends JpaRepository <Postagem, Long>{
 
-	
-
+	List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 	
 
 }
